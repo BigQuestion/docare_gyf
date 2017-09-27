bfa862fec2c47d0851976ef35c53e377a3872d5c
chenyang <137485352@qq.com>
2017-09-27 17:04:28 +0800

    1800

--- src/components/operationMaster/operationMaster.vue
+++ src/components/operationMaster/operationMaster.vue
@@ -1,7 +1,7 @@
 <template>
 	<div style="height:100%;position:relative;">
 		<div class="head">
-            <div class="logo">logo</div>
+            <div class="logo">{{nowTime}}</div>
             <div class="currentPatientInfo">currentPatientInfo</div>
             <div class="procedure">procedure</div>
         </div>
@@ -12,10 +12,24 @@
             <div class="content">
                 <div class="patientList">
                     <div>
-                        <input v-model="dateTime" type="" name="">
+                        <input v-model="getTime" type="" name="">
                     </div>
                     <div>
                         <button @click='searchPatientList'>搜索</button>
+                    </div>
+                    <div>
+                        <div style="display: flex;">
+                            <div>
+                                <input type="radio" id="one" value="10" v-model="operStatus">
+                                <label for="one">术前</label>
+                                <input type="radio" id="two" value="15" v-model="operStatus">
+                                <label for="two">术中</label>
+                                <input type="radio" id="two" value="25" v-model="operStatus">
+                                <label for="two">术后</label>
+                                <br>
+                                
+                            </div>
+                        </div>
                     </div>
                     <div v-for="item in patientList" class="listBorder" v-on:click="patientDeatilInfo(item)">
                         <!-- <div>{{item.patientName}}</div> -->
@@ -30,8 +44,157 @@
                         </ul>
                     </div>
                 </div>
-                <div class="patientInfo">
+                <div class="patientInfo" v-if="viewInfo">
                     <div class="pat_title">患者详情</div>
+                    <div style="margin-top: 5px;">
+                        <div style="border:1px solid rgb(177,207,243);padding-left: 30px;">基本信息</div>
+                        <div style="padding:15px 5px 10px 40px;border-bottom:1px solid rgb(177,207,243);">
+                            <div class="container">
+                                <div>患者ID</div>
+                                <div class="in_con">
+                                    {{patientInfo.PATIENT_ID}}
+                                </div>
+                                <div class="left15">住院号</div>
+                                <div class="in_con">
+                                    {{patientInfo.INP_NO}}
+                                </div>
+                                <div class="left15">姓名</div>
+                                <div class="in_con">
+                                    {{patientInfo.NAME}}
+                                </div>
+                                <div class="left15">年龄</div>
+                                <div class="in_con">
+                                    {{patientInfo.PAT_AGE}}
+                                </div>
+                                <div class="left15">性别</div>
+                                <div class="in_con">
+                                    {{patientInfo.SEX}}
+                                </div>
+                            </div>
+                            <div class="container">
+                                <div>床号</div>
+                                <div class="in_con">
+                                    {{patientInfo.BED_NO}}
+                                </div>
+                                <div class="left15">所在科室</div>
+                                <div class="in_con">
+                                    {{patientInfo.DEPT_NAME}}
+                                </div>
+                                <div class="left15">付费方式</div>
+                                <div class="in_con">
+                                    {{patientInfo.NAME}}
+                                </div> 
+                            </div>
+                            <div class="container">
+                                <div>住址</div>
+                                <div class="in_con" style="width: 600px;">{{patientInfo.MAILING_ADDRESS}}</div>
+                            </div>
+                            <div class="container">
+                                <div>身份证号</div>
+                                <div class="in_con">
+                                    {{patientInfo.ID_NO}}
+                                </div>
+                                <div class="left15">联系电话</div>
+                                <div class="in_con">
+                                    {{patientInfo.NEXT_OF_KIN_PHONE}}
+                                </div>
+                                <div class="left15">联系人</div>
+                                <div class="in_con">
+                                    {{patientInfo.NEXT_OF_KIN}}
+                                </div> 
+                            </div>
+                            <div class="container">
+                                <div>职业</div>
+                                <div class="in_con" style="width: 300px;"> </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="margin-top: 5px;">
+                        <div style="border:1px solid rgb(177,207,243);padding-left: 30px;">手术信息</div>
+                        <div style="padding:15px 5px 10px 40px;border-bottom:1px solid rgb(177,207,243);">
+                            <div class="container">
+                                <div>主要诊断</div>
+                                <div class="in_con" style="width: 600px;">{{patientInfo.DIAG_BEFORE_OPERATION}}</div>
+                            </div>
+                            <div class="container">
+                                <div>病情</div>
+                                <div class="in_con" style="width: 600px;">{{patientInfo.PATIENT_CONDITION}}</div>
+                            </div>
+                            <div class="container">
+                                <div>手术名称</div>
+                                <div class="in_con" style="width: 600px;">{{patientInfo.OPERATION_NAME}}</div>
+                                <div class="left15">急诊择期</div>
+                                <div class="in_con">
+                                    {{patientInfo.NEXT_OF_KIN}}
+                                </div>
+                            </div>
+                            <div class="container">
+                                <div>手术时间</div>
+                                <div class="in_con" style="width: 200px;">{{patientInfo.SCHEDULED_DATE_TIME}}</div>
+                                <div class="left15">台次</div>
+                                <div class="in_con">
+                                    {{patientInfo.SEQUENCE}}
+                                </div>
+                                <div class="left15">手术间</div>
+                                <div class="in_con">
+                                    {{patientInfo.OPERATING_ROOM_NO}}
+                                </div>
+                            </div>
+                            <div class="container">
+                                <div>隔离</div>
+                                <div class="in_con">{{patientInfo.ISOLATION_INDICATOR}}</div>
+                                <div class="left15">手术等级</div>
+                                <div class="in_con">
+                                    {{patientInfo.OPERATION_SCALE}}
+                                </div>
+                                <div class="left15">麻醉方法</div>
+                                <div class="in_con">
+                                    {{patientInfo.ANESTHESIA_METHOD}}
+                                </div>
+                            </div>
+                            <div class="container">
+                                <div>切口等级</div>
+                                <div class="in_con">{{patientInfo.QIEKOU_CLASS}}</div>
+                                <div class="left15">切口个数</div>
+                                <div class="in_con">
+                                    {{patientInfo.QIEKOU_NUMBER}}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="margin-top: 5px;">
+                        <div style="border:1px solid rgb(177,207,243);padding-left: 30px;">手术人员</div>
+                        <div style="padding:15px 5px 10px 40px;border-bottom:1px solid rgb(177,207,243);">
+                            <div class="container">
+                                <div>麻醉医师</div>
+                                <div class="in_con100">{{patientInfo.ANESTHESIA_DOCTOR_NAME}}</div>
+                                <div class="in_con100">{{patientInfo.ANESTHESIA_ASSISTANT_NAME}}</div>
+                                <div class="in_con100">{{patientInfo.THIRD_ANESTHESIA_DOCTOR_NAME}}</div>
+                                <div class="left30">灌注医师</div>
+                                <div class="in_con">
+                                    {{patientInfo.QIEKOU_NUMBER}}
+                                </div>
+                            </div>
+                            <div class="container">
+                                <div>手术医师</div>
+                                <div class="in_con100">{{patientInfo.SURGEON_NAME}}</div>
+                                <div class="left30">手术助手</div>
+                                <div class="in_con100">{{patientInfo.FIRST_ASSISTANT_NAME}}</div>
+                                <div class="in_con100">{{patientInfo.SECOND_ASSISTANT_NAME}}</div>
+                                <div class="in_con100">{{patientInfo.THIRD_ASSISTANT_NAME}}</div>
+                                <div class="in_con100">{{patientInfo.FOURTH_ASSISTANT_NAME}}</div>
+                            </div>
+                            <div class="container">
+                                <div>洗手护士</div> 
+                                <div class="in_con100">{{patientInfo.FIRST_OPERATION_NURSE_NAME}}</div>
+                                <div class="in_con100">{{patientInfo.SECOND_OPERATION_NURSE_NAME}}</div>
+                                <div class="left30">巡回护士</div>
+                                <div class="in_con100">{{patientInfo.FIRST_SUPPLY_NURSE_NAME}}</div>
+                                <div class="in_con100">{{patientInfo.SECOND_SUPPLY_NURSE_NAME}}</div>
+                                <div class="in_con100">{{patientInfo.THIRD_SUPPLY_NURSE_NAME}}</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -40,7 +203,7 @@
                 
             </div>
         </div>-->
-        
+
 	</div>
 </template>
 <script>
@@ -48,14 +211,23 @@ export default {
 	data () {
   		return {
             patientList:[],
-            date:'2014/07/08'
+            viewInfo:false,
+            patientInfo:{},
+            nowTime:"",
+            dateTime:"",
+            count:"",
+            getTime:"2014/07/08",
+            operStatus:""
+
     	}
     },
     methods:{
         searchPatientList(){
+
             let params = {
                 count:10,
-                dateTime:this.date,
+                dateTime:this.getTime,
+                operStatus:this.operStatus
             }
             this.api.getMedOperationMasterList(params)
             .then(
@@ -65,21 +237,60 @@ export default {
 
         },
         patientDeatilInfo(item){
+
             let params = {
                 patientId:item.patientId,
                 operId:item.operId,
                 visitId:item.visitId
-
             }
              this.api.getPatienDetailInfo(params)
              .then(
                 res=>{
-                    this.patientInfo = res.list;
+                    this.patientInfo = res;
                 });
-        }
+             this.viewInfo=true;
+        },
+        setIntervaled(){
+            var _this = this;
+            setInterval(function() {
+            var dateObj = new Date(); //表示当前系统时间的Date对象
+            var year = dateObj.getFullYear(); //当前系统时间的完整年份值
+            var month = dateObj.getMonth() + 1; //当前系统时间的月份值
+            var date = dateObj.getDate(); //当前系统时间的月份中的日
+            var day = dateObj.getDay(); //当前系统时间中的星期值 
+            var hour = dateObj.getHours(); //当前系统时间的小时值
+            var minute = dateObj.getMinutes(); //当前系统时间的分钟值
+            var second = dateObj.getSeconds(); //当前系统时间的秒钟值
+            //如果月、日、小时、分、秒的值小于10，在前面补0
+            if(month < 10) {
+                month = "0" + month;
+            }
+            if(date < 10) {
+                date = "0" + date;
+            }
+            if(hour < 10) {
+                hour = "0" + hour;
+            }
+            if(minute < 10) {
+                minute = "0" + minute;
+            }
+            if(second < 10) {
+                second = "0" + second;
+            }
+            var time = new Date();
+            // 程序计时的月从0开始取值后+1
+            var m = time.getMonth() + 1;
+            var t = year + "-" + month + "-" +
+                day + " " + time.getHours() + ":" +
+                minute + ":" + second;
+            _this.nowTime = t;
+        }, 1000);
+        },
+
     },
     mounted(){
         this.searchPatientList();
+        this.setIntervaled();
     },
     components: {
         
@@ -112,7 +323,7 @@ export default {
 }
 .logo{
     width:150px;
-    background: #0080FF;
+    /*background: #0080FF;*/
 }
 .currentPatientInfo{
     width:150px;
@@ -149,8 +360,34 @@ export default {
     width: calc(100% - 350px);
 }
 .pat_title{
-    height: 50px;
-
+    height: 35px;
+    line-height: 35px;
+    padding-left: 15px;
     border: 1px solid rgb(177,207,243);
+}
+.container{
+    display: -webkit-box; /* Chrome 4+, Safari 3.1, iOS Safari 3.2+ */  
+    display: -moz-box; /* Firefox 17- */  
+    display: -webkit-flex; /* Chrome 21+, Safari 6.1+, iOS Safari 7+, Opera 15/16 */  
+    display: -moz-flex; /* Firefox 18+ */  
+    display: -ms-flexbox; /* IE 10 */  
+    display: flex; /* Chrome 29+, Firefox 22+, IE 11+, Opera 12.1/17/18, Android 4.4+ */  
+    margin-bottom: 15px;
+}
+.in_con{
+    width: 150px;
+    border:1px solid rgb(197,197,197);
+    margin-left: 10px;
+}
+.left15{
+    margin-left: 15px;
+}
+.in_con100{
+    width: 100px;
+    border:1px solid rgb(197,197,197);
+    margin-left: 10px;
+}
+.left30{
+    margin-left: 30px;
 }
 </style>
