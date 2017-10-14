a63a6da240a0f75d9966e441f84d16d75d3cb938
zhang419 <419like@gmail.com>
2017-10-15 00:14:35 +0800

    加入多选

--- src/components/formDesigner/formDesigner.vue
+++ src/components/formDesigner/formDesigner.vue
@@ -1,21 +1,18 @@
 <template>
-	<div>
-		<div class="flex" >
-            <div class="leftButtons" >
+    <div>
+        <div class="flex">
+            <div class="leftButtons">
                 <div v-for="btn in btns" class="btn" draggable="true" @dragstart="drag($event,btn)">
                     {{btn.text}}
                 </div>
             </div>
             <div>
-                <div class="designArea" ref="area" @dragover.prevent @drop="drop" @click="clearClick($event)" @mousedown="areaMouseDown($event)">
-                    <div class="item"  style="position:absolute;min-height: 3px;min-width:3px;" :class="{choosed:item.chosen}" v-for="item in formItems" :style="{left:item.x+'px',top:item.y+'px'}" @click.stop="itemChoose($event,item);" @mousedown.stop="itemMouseDown">
+                <div class="designArea" ref="area" @dragover.prevent @drop="drop" @mousedown="areaMouseDown($event)">
+                    <div class="item" style="position:absolute;min-height: 3px;min-width:3px;" :class="{choosed:item.chosen}" v-for="item in formItems" :style="{left:item.x+'px',top:item.y+'px'}" @click.stop="" @mousedown.stop="itemMouseDown($event,item)">
                         <form-element :value="item"></form-element>
                     </div>
-                    <!-- <div class="item"  style="position:absolute;min-height: 3px;min-width:3px;" :class="{choosed:item.chosen}" draggable="true" v-for="item in formItems" :style="{left:item.x+'px',top:item.y+'px'}" @dragstart.stop="itemDrag($event,item)" @click.stop="itemChoose($event,item);" @mousedown.stop="itemMouseDown">
-                        <form-element :value="item"></form-element>
-                    </div> -->
                     <div class="mask">
-                        <div v-if="chooseRect.endX" style="background:rgba(0,0,0,0.3);position:absolute;" :style="{left:chooseRect.startX+'px',top:chooseRect.startY+'px',width:(chooseRect.endX-chooseRect.startX)+'px',
+                        <div v-if="drawing" style="background:rgba(0,0,0,0.3);position:absolute;" :style="{left:chooseRect.startX+'px',top:chooseRect.startY+'px',width:(chooseRect.endX-chooseRect.startX)+'px',
                         height:(chooseRect.endY-chooseRect.startY)+'px'}"></div>
                     </div>
                 </div>
@@ -34,183 +31,244 @@
                     <input type="" name="" v-model="chooseItems[0].height">
                 </div>
             </div>
-		</div>
-	</div>
+        </div>
+    </div>
 </template>
 <script>
 import formElement from '@/components/formElement/formElement.vue';
 export default {
     name: 'login',
-	data () {
-  		return {
-            formItems:[],
-            btns:[{
-                text:'文本',
-                type:'text',
-                value:'输入文字'
-            },{
-                text:'输入',
-                type:'input'
-            },{
-                text:'单选',
-                type:'radio'
-            },{
-                text:'多选',
-                type:'checkbox'
-            },{
-                text:'横线',
-                type:'line',
-                width:'100'
-            },{
-                text:'竖线',
-                type:'verticalLine',
-                height:'100'
+    data() {
+        return {
+            formItems: [],
+            btns: [{
+                text: '文本',
+                type: 'text',
+                value: '输入文字'
+            }, {
+                text: '输入',
+                type: 'input'
+            }, {
+                text: '单选',
+                type: 'radio'
+            }, {
+                text: '多选',
+                type: 'checkbox'
+            }, {
+                text: '横线',
+                type: 'line',
+                width: '100'
+            }, {
+                text: '竖线',
+                type: 'verticalLine',
+                height: '100'
             }],
-            handleItem:{},
-            offsetX:'',
-            offsetY:'',
-            chooseItems:[],
-            chooseRect:{},
-            area:'',
-            chooseMode:false,
-            domItems:[],
-    	}
+            handleItem: {},
+            offsetX: '',
+            offsetY: '',
+            chooseItems: [],
+            chooseRect: {},
+            area: '',
+            chooseMode: false,
+            domItems: [],
+            currentItem: {},
+            startX: '',
+            startY: '',
+            dragX: '',
+            dragY: '',
+            drawing: false,
+        }
     },
-    methods:{
-        itemMouseDown(e){
-            // this.area.addEventListener('mousemove',itemMove);
-            // this.area.addEventListener('mouseup',itemStop);
+    methods: {
+        itemClick() {
+
         },
-        keyD(e){
-            if(e.key=='Shift'){
+        itemMouseDown(e, currentItem) {
+            this.dragX = e.clientX;
+            this.dragY = e.clientY;
+            this.offsetX = e.offsetX;
+            this.offsetY = e.offsetY;
+            this.$set(this.$data, 'startX', e.clientX);
+            this.$set(this.$data, 'startY', e.clientY);
+            this.currentItem = currentItem;
+            this.area.addEventListener('mousemove', this.areaMouseMove);
+            this.area.addEventListener('mouseup', this.areaMouseUp);
+        },
+        keyD(e) {
+            if (e.key == 'Shift') {
                 this.chooseMode = true;
             }
-            console.log(e);
         },
-        keyU(e){
-            if(e.key=='Shift'){
+        keyU(e) {
+            if (e.key == 'Shift') {
                 this.chooseMode = false;
             }
-            console.log(e);
         },
-        areaMouseMove(e){
-            console.log('move');
-            this.$set(this.$data.chooseRect,'endX',e.clientX-200);
-            this.$set(this.$data.chooseRect,'endY',e.clientY);
-        },
-        areaMouseUp(e){
-            this.area.removeEventListener('mousemove',this.areaMouseMove);
-            this.chooseRect = {};
-        },
-        areaMouseDown(e){
-            this.area = this.$refs.area;
-            this.area.addEventListener('mousemove',this.areaMouseMove);
-            this.area.addEventListener('mouseup',this.areaMouseUp);
+        areaMouseMove(e) {
+            let dX = e.clientX - this.startX;
+            let dY = e.clientY - this.startY;
 
-            this.chooseRect.startX = e.clientX-200;
-            this.chooseRect.startY = e.clientY;
+            for (var i = 0; i < this.chooseItems.length; i++) {
+                this.chooseItems[i].x = this.chooseItems[i].x + dX;
+                this.chooseItems[i].y = this.chooseItems[i].y + dY;
+            }
+            this.$set(this.$data, 'startX', e.clientX);
+            this.$set(this.$data, 'startY', e.clientY);
         },
-        clearClick(e){
-            console.log('click');
+        areaMouseUp(e) {
+            if (this.dragX == e.clientX && this.dragY == e.clientY) {
+                console.log('click');
+                this.itemChoose(e, this.currentItem)
+            }
+            this.area.removeEventListener('mousemove', this.areaMouseMove);
+            this.area.removeEventListener('mouseup', this.areaMouseUp);
+            // let dX = e.clientX-this.startX;
+            // let dY = e.clientY-this.startY;
+            // let chooseItems = this.chooseItems;
+            // for (var i = 0; i < chooseItems.length; i++) {
+            //     chooseItems[i].x += dX;
+            //     chooseItems[i].y += dY;
+            // }
+            console.log(JSON.stringify(this.chooseItems));
+            this.currentItem = {};
+
+        },
+        areaMouseDown(e) {
+            this.chooseItems = [];
+            this.drawing = true;
+            this.drawStartX = e.clientX;
+            this.drawStartY = e.clientY;
+            this.area.addEventListener('mousemove', this.drawMove);
+            this.area.addEventListener('mouseup', this.drawEnd);
+            // this.chooseRect.startX = e.clientX-200;
+            // this.chooseRect.startY = e.clientY;
+
+            this.$set(this.$data.chooseRect, 'startX', e.clientX - 200);
+            this.$set(this.$data.chooseRect, 'startY', e.clientY);
+        },
+        drawMove(e) {
+            this.$set(this.$data.chooseRect, 'endX', e.clientX - 200);
+            this.$set(this.$data.chooseRect, 'endY', e.clientY);
+            // this.chooseRect.endX = e.clientX-200;
+            // this.chooseRect.endY = e.clientY;
+        },
+        drawEnd(e) {
+            if(this.drawStartX == e.clientX&&this.drawStartY == e.clientY){
+                this.clearClick(e);
+            }
+            for (var i = 0; i < this.formItems.length; i++) {
+                if ((this.formItems[i].x > this.chooseRect.startX) && (this.formItems[i].x < this.chooseRect.endX)
+                    &&(this.formItems[i].y > this.chooseRect.startY) && (this.formItems[i].y < this.chooseRect.endY)) {
+                    this.chooseItems.push(this.formItems[i]);
+                    console.log(this.formItems[i].chosen);
+                    this.formItems[i].chosen = true;
+                }
+            }
+            this.chooseRect = {};
+            this.drawing = false;
+            this.area.removeEventListener('mousemove', this.drawStart);
+            this.area.removeEventListener('mouseup', this.drawEnd);
+            console.log(this.chooseRect);
+
+        },
+        clearClick(e) {
+            // console.log('click');
             this.chooseItems = [];
             for (var i = this.formItems.length - 1; i >= 0; i--) {
+                debugger
                 this.formItems[i].chosen = false;
             }
             e.preventDefault();
         },
-        itemChoose(e,item){
-            if(this.chooseMode){
+        itemChoose(e, item) {
+            if (this.chooseMode) {
                 this.chooseItems.push(item);
-                debugger
-                this.domItems.push(e.currentTarget)
+                console.log('长度:' + this.chooseItems.length);
+                // debugger
+                // this.domItems.push(e.currentTarget)
                 item.chosen = true;
                 e.preventDefault();
-            }else{
-                if(this.chooseItems[0]){
+            } else {
+                if (this.chooseItems[0]) {
+                    debugger
                     this.chooseItems[0].chosen = false;
                 }
                 this.chooseItems = [item];
-                this.domItems = [e.currentTarget];
+                // this.domItems = [e.currentTarget];
                 item.chosen = true;
             }
         },
-        itemDrag(e,item){
-            this.handleItem = item;
-            this.offsetX = e.offsetX;
-            this.offsetY = e.offsetY;
-        },
-        itemDragEnd(e,item){
-            // debugger
-            // item.x = e.offsetX;
-            // item.y = e.offsetY;
-            // e.preventDefault();
-        },
-        drag(e,btn){
+        drag(e, btn) {
             // debugger
             let obj = JSON.stringify(btn);
-            this.offsetX = e.offsetX;
-            this.offsetY = e.offsetY;
-            e.dataTransfer.setData('data',obj);
+            e.dataTransfer.setData('data', obj);
         },
         drop: function(e) {
-            if(this.handleItem.type){
-                this.handleItem.x = e.clientX-200-this.offsetX;
-                this.handleItem.y = e.clientY-this.offsetY;
+            if (this.handleItem.type) {
+                this.handleItem.x = e.clientX - 200 - this.offsetX;
+                this.handleItem.y = e.clientY - this.offsetY;
                 this.handleItem = {};
-            }else{
+            } else {
                 let data = JSON.parse(e.dataTransfer.getData('data'));
-                data.x = e.clientX-200-this.offsetX;
-                data.y = e.clientY-this.offsetY;
+                data.x = e.clientX - 200 - this.offsetX;
+                data.y = e.clientY - this.offsetY;
                 this.formItems.push(data);
                 this.handleItem = {};
             }
         }
     },
-    components:{
+    components: {
         formElement
     },
-    mounted(){
-        
+    mounted() {
+        this.area = this.$refs.area;
     },
-    created(){
+    created() {
         let component = this;
-        document.onkeydown = function(e){
+        document.onkeydown = function(e) {
             component.keyD(e);
         }
-        document.onkeyup = function(e){
+        document.onkeyup = function(e) {
             component.keyU(e);
         }
     }
 }
+
 </script>
 <style type="text/css" scoped>
-.mask{
+.mask {
     position: absolute;
-    height:100%;
+    height: 100%;
     width: 100%;
-    background: rgba(0,0,0,0);
+    background: rgba(0, 0, 0, 0);
     pointer-events: none;
 }
-.item.choosed{
-    border:1px dashed #000;
+
+.item.choosed {
+    border: 1px dashed #000;
 }
-.item:hover{
+
+.item:hover {
     background: #E6E6E6;
-    cursor:pointer;
+    cursor: pointer;
 }
-.leftButtons{
-    width:200px;
+
+.leftButtons {
+    width: 200px;
 }
-.btn{
+
+.btn {
     background: #00FFFF;
     margin-top: 10px;
-    width:50px;
+    width: 50px;
     text-align: center;
 }
-.designArea{
+
+.designArea {
     position: relative;
     background: #CCCCCC;
-    height:600px;
-    width:600px;
+    height: 600px;
+    width: 600px;
 }
+
 </style>
