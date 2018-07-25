3d2a91d9ff9d4313e2e782be569f1e8ec0a5b4a7
chenyang <137485352@qq.com>
2018-07-25 09:36:54 +0800

    0936

--- src/fetch/api.js
+++ src/fetch/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-axios.defaults.baseURL = 'http://182.61.36.247:8080';
+// axios.defaults.baseURL = 'http://182.61.36.247:8080';
 axios.defaults.baseURL = 'http://111.111.111.156:8080';
 // axios.defaults.baseURL = 'http://localhost:8088';
 
